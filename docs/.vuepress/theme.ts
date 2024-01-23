2fe42046d032edd0300ae67d83abbc3a455cc074
zhang <821949267@qq.com>
2024-01-23 15:17:06 +0800

    fix

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -5,20 +5,15 @@ import { enSidebar, zhSidebar } from "./sidebar/index.js";
 export default hopeTheme(
   {
     hostname: "https://xiao-zhang-923.github.io/EASE_PAAS_DOCS/",
-
     author: {
       name: "北京德风新征程科技股份有限公司",
       url: "http://www.deltaphone.com.cn/index",
     },
-
     iconAssets: "fontawesome-with-brands",
-
     // logo: "/logo.svg",
-
-    // repo: "vuepress-theme-hope/online-demo",
-
+    repo: "xiao-zhang-923/EASE_PAAS_DOCS",
     docsDir: "docs",
-
+    docsBranch: 'master',
     locales: {
       "/en/": {
         // navbar
@@ -30,7 +25,6 @@ export default hopeTheme(
           editLink: "Edit this page on GitHub",
         },
       },
-
       /**
        * Chinese locale config
        */
@@ -52,62 +46,49 @@ export default hopeTheme(
        
       },
     },
-
-    // plugins: {
+    plugins: {
     //   // You should generate and use your own comment service
-    //   comment: {
-    //     provider: "Giscus",
-    //     repo: "vuepress-theme-hope/giscus-discussions",
-    //     repoId: "R_kgDOG_Pt2A",
-    //     category: "Announcements",
-    //     categoryId: "DIC_kwDOG_Pt2M4COD69",
-    //   },
+      comment: {
+        provider: "Giscus",
+        repo: "vuepress-theme-hope/giscus-discussions",
+        repoId: "R_kgDOLH5X6g",
+        category: "Announcements",
+        categoryId: "DIC_kwDOLH5X6s4Ccnmo",
+      },
 
-    //   // All features are enabled for demo, only preserve features you need here
-    //   mdEnhance: {
-    //     align: true,
-    //     attrs: true,
-
-
-    //     codetabs: true,
-    //     demo: true,
-
-
-    //     figure: true,
-
-     
-    //     imgLazyload: true,
-    //     imgSize: true,
-    //     include: true,
-
-       
-    //     mark: true,
-
-
-    //     playground: {
-    //       presets: ["ts", "vue"],
-    //     },
-
-
-    //     stylize: [
-    //       {
-    //         matcher: "Recommended",
-    //         replacer: ({ tag }) => {
-    //           if (tag === "em")
-    //             return {
-    //               tag: "Badge",
-    //               attrs: { type: "tip" },
-    //               content: "Recommended",
-    //             };
-    //         },
-    //       },
-    //     ],
-    //     sub: true,
-    //     sup: true,
-    //     tabs: true,
-    //     vPre: true,
-    //   },
-    // },
+      // All features are enabled for demo, only preserve features you need here
+      mdEnhance: {
+        align: true,
+        attrs: true,
+        codetabs: true,
+        demo: true,
+        figure: true,
+        imgLazyload: true,
+        imgSize: true,
+        include: true,
+        mark: true,
+        playground: {
+          presets: ["ts", "vue"],
+        },
+        stylize: [
+          {
+            matcher: "Recommended",
+            replacer: ({ tag }) => {
+              if (tag === "em")
+                return {
+                  tag: "Badge",
+                  attrs: { type: "tip" },
+                  content: "Recommended",
+                };
+            },
+          },
+        ],
+        sub: true,
+        sup: true,
+        tabs: true,
+        vPre: true,
+      },
+    },
   },
   { custom: true }
 );
